40f5271a780693a857f2277d294122d2d68d8202
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-06-09 11:53:27 -0500

    feat: config page to make it a PWA

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,18 @@ import './index.css';
 import router from './router/index.tsx';
 import { ThemeProvider } from './context/ThemeContext.tsx';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
